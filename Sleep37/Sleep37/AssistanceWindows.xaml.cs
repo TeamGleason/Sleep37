3b99a48c9a1788f0f304c2ff27ae2aa4b01d61e3
Jon Campbell <joncamp@microsoft.com>
2017-07-26 10:46:13 -0700

    Initial commit for skeleton of the app

---
+++ Sleep37/Sleep37/AssistanceWindows.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using Microsoft.HandsFree.Mouse;
+
+namespace Sleep37
+{
+    /// <summary>
+    /// Interaction logic for AssistanceWindows.xaml
+    /// </summary>
+    public partial class AssistanceWindows : Window
+    {
+        public AssistanceWindows()
+        {
+            InitializeComponent();
+        }
+
+        private void AssistanceWindows_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            GazeMouse.Attach(this);
+        }
+
+        private void AssistanceWindows_OnClosing(object sender, CancelEventArgs e)
+        {
+        }
+
+        private void BackButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Sleep37/Sleep37/WakeWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using Microsoft.HandsFree.Mouse;
+
+namespace Sleep37
+{
+    /// <summary>
+    /// Interaction logic for WakeWindow.xaml
+    /// </summary>
+    public partial class WakeWindow : Window
+    {
+        public WakeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WakeWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            GazeMouse.Attach(this);
+        }
+
+        private void WakeWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+        }
+
+        private void BackButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void CalibrateButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            GazeMouse.LaunchRecalibration();
+        }
+
+        private void WakeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var app = (App)Application.Current;
+            app.Shutdown();
+        }
+
+        private void AssistanceButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var assistanceWindow = new AssistanceWindows();
+            assistanceWindow.ShowDialog();
+        }
+    }
+}
